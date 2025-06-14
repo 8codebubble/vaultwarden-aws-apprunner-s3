6ec7f828c7036a92fa5e95aafd3d57afea87e14d
8codebubble <8codebubble@gmail.com>
2025-06-14 12:47:59 +0000

    updaed rclone command

--- rclone_sync.sh
+++ rclone_sync.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Ensure the data folder exists
 while true; do
-  rclone sync ${DATA_FOLDER} s3remote:vaultwarden-aws-apprunner-s3-bucket --exclude "*.sqlite*" --exclude "*.sqlite3/"
+  rclone sync ${DATA_FOLDER} s3remote:vaultwarden-aws-apprunner-s3-bucket/data-backup \
+    --exclude "*.sqlite*" \
+    --exclude "*.sqlite3/"
   if [ $? -ne 0 ]; then
     echo "rclone Sync failed. Retrying in 2 minutes..."
   else
