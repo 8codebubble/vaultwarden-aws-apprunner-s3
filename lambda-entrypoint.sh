b97557ce161c1f0c6ad8af5ede7979941b3cf6dd
8codebubble <8codebubble@gmail.com>
2025-06-14 12:42:20 +0000

    removed litestream snapshot command

--- lambda-entrypoint.sh
+++ lambda-entrypoint.sh
@@ -16,7 +16,9 @@ fi
 
 # Restore non-SQLite files from S3
 echo "Restoring non-SQLite files from S3..."
-rclone sync s3remote:vaultwarden-aws-apprunner-s3-bucket ${DATA_FOLDER} --exclude "*.sqlite*" --exclude "*.sqlite3/"
+rclone sync s3remote:vaultwarden-aws-apprunner-s3-bucket/data-backup ${DATA_FOLDER} \
+  --exclude "*.sqlite*" \
+  --exclude "*.sqlite3/"
 
 # Ensure the data directory is writable
 ls -la ${DATA_FOLDER}
@@ -29,8 +31,17 @@ RCLONE_PID=$!
 
 
 # Restore SQLite database from S3 if available
+echo "Restoring SQLite database from S3..."
+# This will restore the database if it exists, or do nothing if it doesn't.
 litestream restore -if-replica-exists ${DATA_FOLDER}/db.sqlite3 &
 LITESTREAM_PID=$!
+
+echo "Waiting for Litestream restore to complete..."
+wait $LITESTREAM_PID
+# Ensure the SQLite database is writable
+chmod 644 ${DATA_FOLDER}/db.sqlite3
+# Ensure the SQLite database is owned by the correct user
+#chown root:root ${DATA_FOLDER}/db.sqlite3
 
 # Start Litestream in background for continuous replication
 litestream replicate -config /etc/litestream.yml &
@@ -50,17 +61,15 @@ function shutdown() {
   wait $VAULTWARDEN_PID
   echo "Vaultwarden stopped."
 
-  echo "Creating final Litestream snapshot of ${DATA_FOLDER}/db.sqlite3..."
-  # This command forces a snapshot, ensuring that WAL segments and final data are saved to S3.
-  litestream snapshot -config /etc/litestream.yml  
-
   echo "Syncing data to S3..."
   # Sync the data folder to S3, excluding SQLite files
   # This ensures that the latest state of the data folder is saved.
   # Note: This will not include the SQLite database file, as it is handled by Litestream.
   kill -SIGTERM $RCLONE_PID
   wait $RCLONE_PID
-  rclone sync ${DATA_FOLDER} s3remote:vaultwarden-aws-apprunner-s3-bucket --exclude "*.sqlite*" --exclude "*.sqlite3/"
+  rclone sync ${DATA_FOLDER} s3remote:vaultwarden-aws-apprunner-s3-bucket/data-backup \
+    --exclude "*.sqlite*" \
+    --exclude "*.sqlite3/"
 
   echo "Stopping Litestream..."
   kill -SIGTERM $LITESTREAM_PID
